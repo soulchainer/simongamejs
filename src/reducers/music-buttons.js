c32d951f6e80fb98c99a0c3be1b8c9bb6fbcc1ef
soulchainer <soulchainer@gmail.com>
2017-03-09 14:42:45 +0100

    Simplify music-buttons.js initialState
    When writting on paper about presentational components, I
    realized isn't necessary to use an object with the button id as
    keys for easy access. That made later looping it more complicated.
    Plus, it doesn't made pretty much easier every state change in the
    reducers.

--- src/reducers/music-buttons.js
+++ src/reducers/music-buttons.js
@@ -1,30 +1,30 @@
 import { MUSIC_BUTTON_ON, MUSIC_BUTTON_OFF } from '../actions/index';
 
-const initialState = {
-  green: {
+const initialState = [
+  {
     id: "green",
     active: false,
   },
-  yellow: {
+  {
     id: "yellow",
     active: false,
   },
-  blue: {
+  {
     id: "blue",
     active: false,
   },
-  red: {
+  {
     id: "red",
     active: false,
   },
-};
+];
 
 export default function musicButtons(state = initialState, action) {
   switch (action.type) {
     case MUSIC_BUTTON_ON:
-      return { ...state, [action.id]: { id: action.id, active: true } };
+      return state.map(btn => ({ ...btn, active: action.id === btn.id}));
     case MUSIC_BUTTON_OFF:
-      return { ...state, [action.id]: { id: action.id, active: false } };
+      return state.map(btn => ({ ...btn, active: false}));
     default:
       return state;
   }
