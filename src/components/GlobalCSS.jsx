1c8d9221383d7d789feef945b92d225ab9b566f8
soulchainer <soulchainer@gmail.com>
2017-04-01 13:28:31 +0200

    Change Root container structure & update packages
    - Routes are (for) now directly inside `Root.jsx`.
    - GlobalCSS also gets imported in `Root.jsx` directly, and now
      it's a simple constant imported, not a component.
    - Update packages to last version

--- src/components/GlobalCSS.jsx
+++ src/components/GlobalCSS.jsx
@@ -1,24 +1,19 @@
-import React from 'react';
 import { colors } from '../constants';
 
-const GlobalCSS = () => (
-  <div>
-    <style jsx global>{`
-      body {
-        margin: 0;
-      }
+const GlobalCSS = `
+  body {
+    margin: 0;
+  }
 
-      #root {
-        align-items: center;
-        background-color: ${colors.black};
-        display: flex;
-        justify-content: center;
-        height: 100vh;
-        margin: 0;
-        width: 100vw;
-      }
-    `}</style>
-  </div>
-);
+  #root {
+    align-items: center;
+    background-color: ${colors.black};
+    display: flex;
+    justify-content: center;
+    height: 100vh;
+    margin: 0;
+    width: 100vw;
+  }
+`;
 
 export default GlobalCSS;
--- src/containers/Root.jsx
+++ src/containers/Root.jsx
@@ -1,12 +1,25 @@
 import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import routes from '../routes';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import GameScreen from '../screens/GameScreen';
+import GameOverScreen from '../screens/GameOverScreen';
+import GlobalCSS from '../components/GlobalCSS';
+import StartScreen from '../screens/StartScreen';
+import SettingsScreen from '../screens/SettingsScreen';
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <Router>
-      {routes}
+      <div>
+        <div>
+          <Route exact path="/" render={StartScreen} />
+          <Route exact path="/game" render={GameScreen} />
+          <Route path="/gameover" render={GameOverScreen} />
+          <Route path="/settings" render={SettingsScreen} />
+        </div>
+        <style jsx global>{`${GlobalCSS}`}</style>
+      </div>
     </Router>
   </Provider>
 );
