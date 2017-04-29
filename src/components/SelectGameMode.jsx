2e6fcd1c100e6c3ab97d13d36467a42aca5da150
Juan Riquelme González <juanriqgon@protonmail.com>
2017-04-29 18:32:41 +0200

    styles(settings): «crossbrowser» <select> style

--- src/components/SelectGameMode.jsx
+++ src/components/SelectGameMode.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { gameModes as modes } from '../constants';
+import { colors, gameModes as modes } from '../constants';
 
 const SelectGameMode = ({ onChange, selected }) => (
   <select
@@ -11,8 +11,31 @@ const SelectGameMode = ({ onChange, selected }) => (
     {modes.map(mode => <option key={mode} value={mode}>{mode}</option>)}
     <style jsx>{`
       .SelectGameMode {
+        -moz-appearance: none;
+        -webkit-appearance: none;
+        background-image: url("static/images/chevron-down.svg");
+        background-position: calc(100% - .5rem);
+        background-repeat: no-repeat;
+        background-size: 1.4rem;
+        border: none;
+        color: ${colors.black};
         font-family: 'Titillium Web', sans-serif;
         font-size: 1rem;
+        font-weight: 700;
+        letter-spacing: .1em;
+        line-height: 2rem;
+        padding: .5rem;
+        text-transform: capitalize;
+      }
+
+      .SelectGameMode:-moz-focusring {
+        color: transparent;
+        text-shadow: 0 0 0 ${colors.black};
+      }
+
+      option {
+        background-color: ${colors.white};
+        font-weight: 400;
       }
     `}</style>
   </select>
